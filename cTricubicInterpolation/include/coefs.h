370166dcd33a5f9d742fc8bbb1c2cfed543f6f9e
kparasch <konstantinos.paraschou@cern.ch>
2019-08-05 17:24:24 +0200

    fix include of stdlib

--- cTricubicInterpolation/include/coefs.h
+++ cTricubicInterpolation/include/coefs.h
@@ -1,6 +1,8 @@
 #ifndef __COEFS_H__
 #define __COEFS_H__
 
+#include <stdlib.h>
+
 double* get_coefs(double* b);
 
 #endif
--- cTricubicInterpolation/include/derivs.h
+++ cTricubicInterpolation/include/derivs.h
@@ -1,6 +1,8 @@
 #ifndef __DERIVS_H__
 #define __DERIVS_H__
 
+#include <stdlib.h>
+
 double* finite_diff(int shape1, int shape2, double A[][shape1][shape2], int ix, int iy, int iz);
 double* exact_diff(int shape1, int shape2, int shape3, double A[][shape1][shape2][shape3], int ix, int iy, int iz, double dx, double dy, double dz);
 
