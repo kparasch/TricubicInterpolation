0444f098e5b51bbef798f43d68fbc2270ad14868
kparasch <konstantinos.paraschou@cern.ch>
2019-08-06 12:32:02 +0200

    fixed typo in type

--- cTricubic/include/evaluate.h
+++ cTricubic/include/evaluate.h
@@ -1,7 +1,7 @@
 #ifndef __EVALUATE_H__
 #define __EVALUATE_H__
 
-double xyz_powers(double xn, double yn, double zn, double* xni, double* ynj, double* znk);
+void xyz_powers(double xn, double yn, double zn, double* xni, double* ynj, double* znk);
 double eval(double* coefs, double* xn, double* yn, double* zn);
 double ddx(double* coefs, double* xn, double* yn, double* zn, double dx);
 double ddy(double* coefs, double* xn, double* yn, double* zn, double dy);
